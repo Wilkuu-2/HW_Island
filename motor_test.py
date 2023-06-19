ceccadb7ed6850dbb6d9cf2e900e4e8c030059d6
Jakub Stachurski <jakub@wilkuu.xyz>
2023-06-19 16:17:17 +0200

    Make motor_test.py a legitemate cli tool

--- motor_test.py
+++ motor_test.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python3 
 import motor
+import argparse
 from time import sleep
 
-m = motor.Motor("/dev/ttyACM0", 9600)
 
-m.sendCMD("ROR 0, 10000")
-sleep(5)
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser( prog = "Motor tester",
+                                     description="Tests the big lift motor",
+                                     epilog='Part of Hybrid worlds project of 2023'
+                                    )
+    parser.add_argument("serial_port", help="The serial interface of the motor")
+    parser.add_argument("baudrate", help="The serial baudrate of the motor interface", default=9600)
+    parser.add_argument("-c","--count", help="Amount of test loops", default=-99)
 
-m.sendCMD("MST 0")
+    args = vars(parser.parse_args())
+    print("Arguments:\n", args)
+    
+    try:
+        count = int(args["count"])
+    except Exception as e:
+        print("Could not parse loop count:\n", e)
+        exit(1)
+
+    try:
+        m = motor.Motor(args["serial_port"], int(args["baudrate"]))
+    except Exception as e:
+        print("Unable to open motor interface:\n", e)
+        exit(1)
+
+    iterc = 0
+
+    while count < 0 or iterc < count:
+        m.sendCMD("ROR 0, 50000")
+        sleep(1)
+
+        m.sendCMD("MST 0")
+        sleep(1)
+
+        m.sendCMD("ROR 0, 50000")
+        sleep(1)
+
+        iterc += 1
