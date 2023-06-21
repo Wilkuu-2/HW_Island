9d1dc7a291f77d991e610f4f9f5496bbd1138e74
Jakub Stachurski <jakub@wilkuu.xyz>
2023-06-21 20:25:08 +0200

    Comment and neaten the query and comms code on the python side

--- sqlite/comms.py
+++ sqlite/comms.py
@@ -1,8 +1,20 @@
+# =======================================================*
+# Simple Arduino communication protocol w/ discovery
+# 
+#
+# 
+# Copyright 2023, Group Tantor (CreaTe M8 2023)
+# You may use the code under the terms of the MIT license
+# =======================================================*/
+
 import serial
 import serial.tools.list_ports
 import sys
 import readline
 
+
+# util function for interactive port selection 
+# it allows us to prefill the input with the first port name to make interactions quicker
 def input_with_prefill(prompt, text):
     def hook():
         readline.insert_text(text)
@@ -12,67 +24,68 @@ def input_with_prefill(prompt, text):
     readline.set_pre_input_hook()
     return result
 
+
+# The serial class which is a wrapper around the pyserial Serial class 
 class Serial:
 
+    # Set up connection 
     def __init__(self, auto_id=None):
         # set up connection
         if auto_id is not None:
             self.serialInst = self.connectionAuto(auto_id)
-        elif sys.platform == 'windows' or sys.platform == 'cygwin':
-            self.serialInst = self.connectionWin()
         else: 
-            self.serialInst = self.connectionLinux()
+            self.serialInst = self.connectionInteractive()
 
-    def connectionWin(self):
-        ports = serial.tools.list_ports.comports()
+        if self.serialInst is None:
+            raise Exception("[Serial] Could not find valid serial port")
+
+             
         
+
+    # Get a connection interactively   
+    def connectionInteractive(self):
+        ports = serial.tools.list_ports.comports()
+        # Handle no devices being connected
         if len(ports) < 1:
             print("[Error] Could not find any serial devices... \nReconnect the arduino and try again.")
-            exit(1)
+            return None
 
         print("== Detected ports: ==")
         for port in ports: 
             print("-->\t", str(port.device))
-
-        comport = input_with_prefill("== Select Com port: ==\n> ", str(ports[0].device))
-
-        for x in range(0, len(portList)):
-            if portList[x].startswith('COM' + str(val)):
-                portVar = 'COM' + str(val)
-
-        ser = serial.serial()
-        ser.baudrate = 9600  # same as arduino
+        
+        path = input_with_prefill("== Give device path/comport: ==\n> ", str(ports[0].device))
+        # Configure serial  
+        ser = serial.Serial()
+        ser.port = path
         ser.timeout = 1
-        ser.port = comport
         ser.open()
         return ser
-
-
-    def connectionLinux(self):
-        ports = serial.tools.list_ports.comports()
-        
-        if len(ports) < 1:
-            print("[Error] Could not find any serial devices... \nReconnect the arduino and try again.")
-            exit(1)
-
-        print("== Detected ports: ==")
-        for port in ports: 
-            print("-->\t", str(port.device))
-        
-        path = input_with_prefill("== Give device path: ==\n> ", str(ports[0].device))
-        return serial.Serial(path,9600)
 
     def connectionAuto(self, id):
         ports = serial.tools.list_ports.comports()
         
+        # Handle no devices being connected
+        if len(ports) < 1:
+            print("[Error] Could not find any serial devices... \nReconnect the arduino and try again.")
+            return None
+
+        # Look trough all ports to find the one 
         for port in ports:
             try:
-                s = serial.Serial(port.device, 9600)
-
-                self.send_line_val('u',id, ser=s)
+                # Create a serial socket  
+                s = serial.Serial()
+                s.port=port.device
+                s.baudrate=9600
+                s.timeout=1
+                s.open()
                 
+                # Send hanshake
+                self.send_message('u',id, ser=s)
+               
+                # Read until a handshake is found  
                 while True: 
-                    l,v = self.wait_for_line_val(ser=s)
+                    l,v = self.wait_for_message(ser=s)
                     if l == 'i':
                         if v == id: 
                             print(f"[AUTO] Port {port.device} is '{id}', target found. ")
@@ -83,38 +96,52 @@ class Serial:
                 s.close()
             except Exception as e: 
                 print(f"[AUTO] Port {port.device} could not be connected to:\n{e}")
+                s.close()
+        
+        print(f"[AUTO] No working ports with id of '{id}'")
+        return None
 
-    def send_line_val(self, label, value, ser=None):
+    def send_message(self, label, value, ser=None):
+        # Optional serial override, used for discovery 
         if ser is None: 
             ser = self.serialInst
 
+        # Write message 
         ser.write(f"{label}{value}\r\n".encode(encoding='ascii', errors='replace'))
         
 
-
-    def wait_for_line_val(self, ser=None):
-        print(ser)
+    # Reads the message, byte by byte 
+    def wait_for_message(self, ser=None):
+        # Optional serial override, used for discovery
         if ser is None: 
             ser = self.serialInst
 
+        # Composes the line  
         line = []
         while True:
              char = str(ser.read(1), 'utf-8')
+             # Line break and carriage return are the breaking characters in this protocol
              if '\n' in char or '\r' in char:
                  break
+            
              line.append(char)
 
+        # Return z-message for stray breaking characters 
         if len(line) < 2:
             return 'z', -1
 
-        try:
+        try: 
+            # If we capture too little characters by accident, this will fail
             val = ''.join(line[1:])
             label = line[0]
         except:
+            # In that case return a z-message 
             return 'z', -1
         
+        # Debug
         print(f"READ: {label=} , {val=}")
         return label,val 
     
+    # Wrapper to close the socket 
     def close(self):
         self.serialInst.close()
--- sqlite/querrieScript.py
+++ sqlite/querrieScript.py
@@ -1,32 +1,49 @@
-import sqlite3
+# =======================================================*
+# Arduino driven sqlite querying 
+# Reads input from serial and prints query based on it
+# uses comms.py as it's communication protocol
+# 
+# Copyright 2023, Group Tantor (CreaTe M8 2023)
+# You may use the code under the terms of the MIT license
+# =======================================================*/
 
+import sqlite3
 import comms
 
+# Mappings 
 Countries = [(0, "Asia"),(1,"Americas"),(2,"Europe"),(3,"Africa")]
 Types = [(0,"Flood"),(1, "Drought"),(2, "Storm")]
 Decades = [(0,1950),(1,1960),(2,1970),(3,1980),(4,1990),(5,2000),(6,2010)]
+# TODO: TAG UUID's for continents 
 
-
-
+# util function, might have to find a home somewhere else  
 def int_or_n1(num):
     try: 
         return int(num)
     except: 
         return -1
 
+# Data getter function
 def get_data(ser):
     dec = None 
     dis = None 
     uuid = None 
 
+    # Listen until found all of the inputs 
     while True:
+        # Reads from the arduino 
         l,v = ser.wait_for_line_val()
+
+        # Decade 
         if l == 'a': 
             dec = int_or_n1(v)
             print(f"{dec=}")
+        # Disaster type 
         elif l == 'b': 
             dis = int_or_n1(v)
             print(f"{dis=}")
+
+        # Continent UUID  
         elif l == 'c':
             uuid = v
             print(f"{uuid=}")
@@ -35,32 +52,27 @@ def get_data(ser):
             return dec,dis, uuid
 
 if __name__ == "__main__":
+    # Connect to database 
     con = sqlite3.connect("emdat_public5.db")
     cur = con.cursor()
+    
+    # Connect to serial 
     s = comms.Serial(auto_id="input")
 
     decade, disaster, uuid = get_data(s)
-    if decade == -1  or disaster == -1:
-        print("no_data")
-        get_data(s)
-
-    #print("input a decade(so 1900, tot 1980, 1990, 2000 or 2010")
-    #x = input()
-    #x = int(x)
-    #y = int(x+10)
+    
+    # TODO: Replace with arduino input 
     print("input a number with the buttonsliderthing for the continent between 0 and 3")
     c = input()
     c = int(c)
-    #print("input a number for the disaster type, (0 = flood, 1 = drought, 2 = storm")
-    #t = input()
-    #t = int(t)
-
-    #decade = x
-    #disaster = t
+    
+    # Map the values 
     in_disaster = Types[disaster][1]
     in_year_start = Decades[decade][1]
     in_year_end = in_year_start + 10 
     in_continent = Countries[c][1] 
+
+    # Debug
     print(f"{in_disaster}s from {in_year_start} to {in_year_end} in {in_continent}")
 
     res = cur.execute("""
@@ -79,6 +91,6 @@ if __name__ == "__main__":
                        str(in_continent),
                        str(in_disaster)))
 
-          
+    # Get the results of the query 
     print(res.fetchall())
     s.close()
