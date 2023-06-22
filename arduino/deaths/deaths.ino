badcb7d5f6aabcd72ad5dca5f526d7ce5435ed9f
voidspace304 <137267133+voidspace304@users.noreply.github.com>
2023-06-22 14:02:03 +0200

    Update deaths.ino comments

--- arduino/deaths/deaths.ino
+++ arduino/deaths/deaths.ino
@@ -12,13 +12,14 @@ void loop() {
   if (Serial.available() > 0) { // check if data is available in the serial buffer
     int death_value = Serial.parseInt(); // read the value from the serial buffer
     Serial.println(death_value);
-    int brightness = map(death_value, 0, 1023, 0, 255); // map the values 0-1023 to a brightness level between 0-255
-    for (int i = 0; i <= NUM_LEDS/2; i++) { // turn the RGB strip red with the mapped brightness level
+    int brightness = map(death_value, 0, 1023, 0, 255); // map the death values (max 1023 atm) to a brightness level between 0-255
+    
+    for (int i = 0; i <= NUM_LEDS; i++) { // turn the RGB strip red with the mapped brightness level
       leds[i] = CRGB(brightness, 0, 0);
-      leds[i+7] = CRGB(255, 0, 0);
       FastLED.show();
     }
-    delay(5000); // wait for 5 seconds
+    delay(5000); // let RGBs shine for 5 seconds
+    
     for (int i = 0; i <= NUM_LEDS; i++) { // turn off the RGB strip
       leds[i] = CRGB(0, 0, 0);
       FastLED.show();
