7dcb8d58873b5739a31faa9518a9bdaf47848938
Jakub Stachurski <jakub@wilkuu.xyz>
2023-06-22 13:50:33 +0200

    fix paths

--- arduino_hanshaking/libs/comm.cpp
+++ arduino_hanshaking/libs/comm.cpp
@@ -21,7 +21,7 @@
  * The Message struct is the base of the protocol:
  * function: {                 Sent Portion                }{Bookkeeping  }
  * var:      [label][content (c style string)              ](string lenght)
- * size:     <1byte><--------MAX_MESSAGE_LENGTH bytes------><----2bytes--->
+ * size:     <1byte><--------MAX_MESSAGE_LENGTH bytes------><----8byte---->
  *
  * example messages: 
  *           [t][Lorem ipsum\0] (12 bytes)
@@ -91,20 +91,22 @@ void send_message(Message m){
 Message wait_for_message(){
   Message m = {0};
 
+  while(Serial.readBytes(&m.label, 1) < 1)
+      delay(5);
+
   while(m.mlen < MAX_MESSAGE_LENGTH){
-    char ch = ' ';
+    char ch[1] = {'X'};
     
     // Because arduino does not allow bytewise reads, we have to gaslight the compiler into thinking that we are reading into a 1 byte long buffer
-    if (Serial.available() && Serial.readBytes((char *)(&ch),1) < 1)
+    if (!Serial.available() || Serial.readBytes(ch,1) < 1)
         continue; // Don't check or write the character when no character recieved
     
     // newlines and carriage returns are the breaking characters for this protocol
-    if(ch == '\n' || ch == '\r'){
+    if(*ch == '\n' || *ch == '\r'){
         break;  
     }
-
     // Write into buffer 
-    m.content[m.mlen] = ch; 
+    m.content[m.mlen] = *ch; 
     m.mlen ++;     
   }
   
@@ -119,8 +121,8 @@ Message wait_for_message(){
 
 // Handshake
 // Responds to hanshake requests and returns true if the request was a handshake request
-inline bool handle_handshake(Message m, const char * id){
-    if(m.label = 'u'){
+bool handle_handshake(Message m, const char * id){
+    if(m.label == 'u'){
         send_message(cstr_message('i',id));
         return true; 
     }
