76350f93b32dbea46dfab29e75f2acaec7f0a9f9
voidspace304 <137267133+voidspace304@users.noreply.github.com>
2023-06-22 14:07:11 +0200

    Update droughts_strip.ino comments

--- arduino/droughts_strip/droughts_strip.ino
+++ arduino/droughts_strip/droughts_strip.ino
@@ -5,16 +5,16 @@ CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  Serial.begin(9600); // initialize serial communication
+  Serial.begin(9600); 
 }
 
 void loop() {
   int defaultGreen = 255;
   int maxRed = 255;
-  int delayTime = random(10, 30);
-  int flickerNumber = 150;
+  int delayTime = random(10, 30); //random time for keeping LEDs on, to simulate fire-like effect
+  int flickerNumber = 150; //number of flickers per drought event
 
-  for (int i = 0; i <= NUM_LEDS; i++) { // turn off the RGB strip
+  for (int i = 0; i <= NUM_LEDS; i++) { // RGB strip color set default to green
           leds[i] = CRGB(0, defaultGreen, 0);
           FastLED.show();
   }
@@ -23,20 +23,20 @@ void loop() {
       int drought_value = Serial.parseInt(); // read the value from the serial buffer
       Serial.println("drought value: " + String(drought_value));
       
-      int greenValue = map(drought_value, 0, 1, defaultGreen, 60); // on or off with drought lights
+      int greenValue = map(drought_value, 0, 1, defaultGreen, 60); // 0 or 1 as input values are mapped to red-green spectrum
       int redValue = map(drought_value, 0, 1, 0, maxRed);
 
-      if (drought_value > 0) {
+      if (drought_value > 0) { //if there is a serial communication input value
         for (int i = 0; i <= flickerNumber; i++) {
-          for (int i = 0; i <= NUM_LEDS; i++) { // turn the RGB strip red with the mapped brightness level
+          for (int i = 0; i <= NUM_LEDS; i++) { // have a fire-like flickering effect by slighly changing the hue of the LEDs
             leds[i] = CRGB(random(redValue-30, redValue+30), random(greenValue-35, greenValue+5), 0);
             FastLED.show();
           }
-          delay(delayTime); // wait for a random time
+          delay(delayTime); // leave the lights on for a random amount of time
         }
 
         
-        for (int i = 0; i <= NUM_LEDS; i++) { // reset strip
+        for (int i = 0; i <= NUM_LEDS; i++) { // reset strip to original color
           leds[i] = CRGB(0, defaultGreen, 0);
           FastLED.show();
         }
