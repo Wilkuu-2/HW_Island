71eaf09564a5ed2d727a95a3814e3cf8ef29e6c1
DimitarYo <118270830+DimitarYo@users.noreply.github.com>
2023-06-23 01:53:58 +0200

    Everything changed or added except lcd screen

--- arduino/deaths/deaths.ino
+++ arduino/deaths/deaths.ino
@@ -1,17 +1,25 @@
 #include <FastLED.h>
 #define LED_PIN     10
 #define NUM_LEDS    15
+#include "libs/comm.cpp"
+
+bool blinkstate = 0; 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   Serial.begin(9600); // initialize serial communication
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+
+#define ID "input"
 
 void loop() {
   if (Serial.available() > 0) { // check if data is available in the serial buffer
-    int death_value = Serial.parseInt(); // read the value from the serial buffer
-    Serial.println(death_value);
+    Message m = wait_for_message();
+    if(!handle_handshake(m,ID)){
+
+    int death_value = atoi(m.content); // read the value from the serial buffer
     int brightness = map(death_value, 0, 1023, 0, 255); // map the death values (max 1023 atm) to a brightness level between 0-255
     
     for (int i = 0; i <= NUM_LEDS; i++) { // turn the RGB strip red with the mapped brightness level
@@ -24,5 +32,12 @@ void loop() {
       leds[i] = CRGB(0, 0, 0);
       FastLED.show();
     }
+    } else { 
+      blinkstate = !blinkstate;
+      digitalWrite(LED_BUILTIN, blinkstate);
+    }
+    
+    
+   
   }
 }
--- arduino/injuries/injuries.ino
+++ arduino/injuries/injuries.ino
@@ -1,36 +1,47 @@
 #include <FastLED.h>
-#define LED_PIN     10
-#define NUM_LEDS    15
-CRGB leds[NUM_LEDS];
+#define LED_PIN 10
+#define NUM_LEDS 15
+#include "libs"
+
+bool blinkstate = 0 : CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available() > 0) { // check if data is available in the serial buffer
-      int injury_value = Serial.parseInt(); // read the value from the serial buffer
-      int injury_index = map(injury_value, 0, 1023, 0, NUM_LEDS); // map the values 0-1023 to an index value to be used for certain number of LEDs
-      Serial.println("input value: " + String(injury_value) + ", index: " + String(injury_index));
+#define ID "input"
 
-      if (injury_value > 0) { //if there is a serial communication value input
-        for (int i = 0; i <= injury_index; i++) { // turn the number of LEDs equivalent to the number of injuries white
+void loop() {
+  if (Serial.available() > 0) {  // check if data is available in the serial buffer
+    Message m = wait_for_message();
+    if (!handle_handshake(m, ID)) {
+      int injury_value = atoi(m.content);                          // read the value from the serial buffer
+      int injury_index = map(injury_value, 0, 1023, 0, NUM_LEDS);  // map the values 0-1023 to an index value to be used for certain number of LEDs
+
+      if (injury_value > 0) {                      //if there is a serial communication value input
+        for (int i = 0; i <= injury_index; i++) {  // turn the number of LEDs equivalent to the number of injuries white
           leds[i] = CRGB(255, 255, 255);
           FastLED.show();
         }
-        delay(5000); // wait for 5 seconds
-        
-        for (int i = 0; i <= injury_index; i++) { // reset the LEDs that were turned on
+        delay(5000);  // wait for 5 seconds
+
+        for (int i = 0; i <= injury_index; i++) {  // reset the LEDs that were turned on
+          leds[i] = CRGB(0, 0, 0);
+          FastLED.show();
+        }
+      } else {
+        for (int i = 0; i <= injury_index; i++) {  // otherwise there is no input, keep strip turned off
           leds[i] = CRGB(0, 0, 0);
           FastLED.show();
         }
       }
-      else {
-        for (int i = 0; i <= injury_index; i++) { // otherwise there is no input, keep strip turned off
-          leds[i] = CRGB(0, 0, 0);
-          FastLED.show();
-        }
-      }  
+      send_message(m);
+    }
+  } else {
+    blinkstate = !blinkstate;
+    digitalWrite(LED_BUILTIN, blinkstate)
+    delay(200);
   }
 }
