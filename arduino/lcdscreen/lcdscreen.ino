cddff0ac28870424ac9353c8e6aecf780bba184c
DimitarYo <118270830+DimitarYo@users.noreply.github.com>
2023-06-23 11:11:24 +0200

    LCD screen done

--- arduino/lcdscreen/lcdscreen.ino
+++ arduino/lcdscreen/lcdscreen.ino
@@ -1,13 +1,33 @@
-/* Hello Wokwi! */
 #include <LiquidCrystal_I2C.h>
+#include "libs/comm.cpp"
+
+bool blinkstate = 0;
+String name_and_temperature = " ";
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 void setup() {
 lcd.init();
 lcd.backlight();
+lcd.setCursor(0, 0);
+lcd.print("The mean temperature for ");
+lcd.setCursor(0,1);
+lcd.print(name_and_temperature);
+
 }
 void loop() {
-lcd.setCursor(0, 0);
-lcd.print("Temperature:");
-lcd.setCursor(0,1);
-lcd.print("46 graden");
+  if(Serial.available()){
+    Message m  = wait_for_message();
+
+    // Respond to the handshakes 
+    if(!handle_handshake(m,ID)){
+      name_and_temperature = m.content
+      lcd.setCursor(0,1);
+      lcd.print(name_and_temperature);
+      send_message(m);
+    }
+  } else {
+    blinkstate = !blinkstate;
+    digitalWrite(LED_BUILTIN, blinkstate);
+    delay(200);  
+  }
 }
