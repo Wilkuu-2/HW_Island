2529888bfca45bf70496ef6ea348b13c049bcb96
DimitarYo <118270830+DimitarYo@users.noreply.github.com>
2023-06-28 14:41:27 +0200

    pushi pushi

--- arduino/injuries/injuries.ino
+++ arduino/injuries/injuries.ino
@@ -3,7 +3,8 @@
 #define LED_PIN_DROUGHTS 11
 #define LED_PIN_INJURIES 10
 #define LED_PIN_DEATHS 9
-#define NUM_LEDS_INJURIES 15
+#define NUM_LEDS_DEATH 15
+#define NUM_LEDS_INJURIES 44
 #define NUM_LEDS_DROUGHTS 15
 
 const int max_value = 255;
@@ -16,13 +17,22 @@ CRGB injuries_leds[NUM_LEDS_INJURIES];
 CRGB deaths_leds[NUM_LEDS_INJURIES];
 CRGB droughts_leds[NUM_LEDS_DROUGHTS];
 
+int injury_value = 0;
+int death_value = 0;
+int drought_value = 0;
+
+int injury_skip[] = {100,200,300}; 
+int iskip_len = 3 
+
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN_INJURIES, GRB>(injuries_leds, NUM_LEDS_INJURIES);
-  FastLED.addLeds<WS2812, LED_PIN_DEATHS, GRB>(deaths_leds, NUM_LEDS_INJURIES);
-  FastLED.addLeds<WS2812, LED_PIN_DROUGHTS, GRB>(droughts_leds, NUM_LEDS_INJURIES);
+  FastLED.addLeds<WS2812, LED_PIN_DEATHS, GRB>(deaths_leds, NUM_LEDS_DEATH);
+  FastLED.addLeds<WS2812, LED_PIN_DROUGHTS, GRB>(droughts_leds, NUM_LEDS_DROUGHTS);
   start_time = millis();
   Serial.begin(9600);
 }
+
+#define ID = "leds" 
 
 void loop() {
   int delay_time_drought = random(10, 30);
@@ -34,21 +44,41 @@ void loop() {
     display_led(NUM_LEDS_DROUGHTS, drougts_leds, min_value, max_value, min_value);
   }
   if (Serial.available() > 0) {
-    current_time = millis();               // check if data is available in the serial buffer
-    int injury_value = Serial.parseInt();  // read the value from the serial buffer
-    int death_value = Serial.parseInt();
-    int drought_value = Serial.parseInt();
+    Message m = wait_for_message()
+    if (handle_handshake(m,ID))
+        return; // check if data is available in the serial buffer
+    
+    switch (m.label){
+      case 'a':
+         injury_value = atoi(m.content);
+      break;
+
+      case 'b':
+         death_value = atoi(m.content);
+      break;
+
+      case 'c': 
+         drought_value = atoi(m.content);
+      break; 
+
+      default:
+
+    }
+
+    // int injury_value = Serial.parseInt();  // read the value from the serial buffer
+    // int death_value = Serial.parseInt();
+    // int drought_value = Serial.parseInt();
 
     int greenValue = map(drought_value, 0, 1, defaultGreen, 60);  // 0 or 1 as input values are mapped to red-green spectrum
     int redValue = map(drought_value, 0, 1, 0, maxRed);
-    int injury_index = map(injury_value, 0, 1023, 0, NUM_LEDS);  // map the values 0-1023 to an index value to be used for certain number of LEDs
+    int injury_index = map(injury_value, 0, 1023, 0, NUM_LEDS_INJURIES);  // map the values 0-1023 to an index value to be used for certain number of LEDs
     int brightness = map(death_value, 0, 1023, 0, 255);
     if (drought_value > 0 && !flickering) {
       flickering = true;
       previousMillis = currentMillis;
     }
-    display_led(NUM_LEDS_INJURIES, deaths_leds, brightness, min_value, min_value)
-      display_led(injury_index, injuries_leds, max_value, max_value, max_value);
+    display_led(NUM_LEDS_DEATHS, deaths_leds, brightness, min_value, min_value)
+    display_led(injury_index, injuries_leds, max_value, max_value, max_value);
     if (flickering) {
       if (currentMillis - previousMillis >= delayTime) {
         previousMillis = currentMillis;
@@ -61,16 +91,29 @@ void loop() {
         }
       }
     }
-    if (current_time - start_time >= stop_interval) {
-      display_led(injury_index, injuries_leds, min_value, min_value, min_value);
-      display_led(NUM_LEDS_INJURIES, deaths_leds, min_value, min_value, max_value);
-      start_time = millis();
-    }
   }
 }
 
 void display_led(int nr_of_leds, CRGB b[], int red_col, int green_col, int blue_col) {
   for (int i = 0; i <= nr_of_leds; i++) {  // turn the number of LEDs equivalent to the number of injuries white
     b[i] = CRGB(red_col, green_col, blue_col);
-    FastLED.show();
   }
+  FastLED.show();
+}
+void display_led_skip(int nr_of_leds, CRGB b[], int red_col, int green_col, int blue_col){
+  for (int i = 0; i <= nr_of_leds; i++) {  // turn the number of LEDs equivalent to the number of injuries white
+    CRGB col = CRGB(red_col, green_col, blue_col)
+    
+    for (int j = 0; j < iskip_len; j++){
+        if (i == injuries_skip[j]){
+           col = CRGB(0,0,0)
+           break
+        }
+        
+    } 
+    
+    b[i] = col;
+  }
+
+  FastLED.show();
+}
